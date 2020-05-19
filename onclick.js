937992facfcdd6deae5fb410c527828d9d29c75b
marzakkak <62803028+marzakkak@users.noreply.github.com>
2020-05-19 14:09:05 +0400

    taking forever

--- onclick.js
+++ onclick.js
@@ -2,25 +2,20 @@ const popupBtn = document.getElementById("popupbtn");
 const popupVideo = document.getElementById("popupvideo");
 const blurBg = document.getElementById("blured-bg");
 const blurContainer = document.getElementById("blur-container");
-const ytVideo = document.getElementById("popupvideo");
-
-ytVideo.innerHTML =
-  "<iframe id='video-music' width='560' height='315' src='https://www.youtube.com/embed/fqRiqWJcess' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+const ytVideo = document.getElementById("video-music");
 
 function openPopup() {
   blurBg.classList.add("visible");
   popupVideo.classList.add("visible");
   blurContainer.classList.add("blur");
   song.pause();
-
-  document.body.appendChild(ytvideo);
+  ytVideo.pause();
 }
 
 function closeBlur() {
   blurBg.classList.remove("visible");
   blurContainer.classList.remove("blur");
   song.play();
-  document.body.removeChild(ytvideo);
 }
 
 blurBg.addEventListener("click", closeBlur);
