f4d3875306781ba5aa0a1471b221020781d616d7
marzakkak <62803028+marzakkak@users.noreply.github.com>
2020-05-19 13:50:30 +0400

    new update 1:50pm

--- onclick.js
+++ onclick.js
@@ -2,26 +2,27 @@ const popupBtn = document.getElementById("popupbtn");
 const popupVideo = document.getElementById("popupvideo");
 const blurBg = document.getElementById("blured-bg");
 const blurContainer = document.getElementById("blur-container");
-const ytVideo = document.getElementById("video-music");
+const ytVideo = document.getElementById("popupvideo");
+
+ytVideo.innerHTML =
+  "<iframe id='video-music' width='560' height='315' src='https://www.youtube.com/embed/fqRiqWJcess' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
 
 function openPopup() {
   blurBg.classList.add("visible");
   popupVideo.classList.add("visible");
   blurContainer.classList.add("blur");
   song.pause();
+
+  document.body.appendChild(ytvideo);
 }
 
 function closeBlur() {
   blurBg.classList.remove("visible");
   blurContainer.classList.remove("blur");
   song.play();
-
-  ytVideo.pause();
 }
 
 blurBg.addEventListener("click", closeBlur);
 popupBtn.addEventListener("click", openPopup);
 
-function pausevideo() {
-  ytVideo.pause();
-}
+function pausevideo() {}
